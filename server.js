72f4ad6dc4a9e6ce6d7bd9ab6d68e9622e78b497
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-10-24 22:28:22 +0300

    basic files listing

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express = require("express");
+import express from "express";
+import fs from "fs";
+const dataFolder = "./data";
+let filesList = [];
 
 const app = express();
 
@@ -6,8 +9,17 @@ app.listen(3000);
 
 app.set("view engine", "ejs");
 
+fs.readdir(dataFolder, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  data.forEach((file) => {
+    filesList.push(file);
+  });
+});
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { filesList });
 });
 
 app.get("/create", (req, res) => {
