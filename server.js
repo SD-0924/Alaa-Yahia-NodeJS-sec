9ff0c19412a2b2b4584a82e67c83a1fd70e224a4
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-10-24 23:49:08 +0300

    basic files creating

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import fs from "fs";
-const dataFolder = "./data";
+import bodyParser from "body-parser";
+
+const dataFolder = "./data/";
 let filesList = [];
 
 const app = express();
@@ -8,22 +10,41 @@ const app = express();
 app.listen(3000);
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-fs.readdir(dataFolder, (err, data) => {
-  if (err) {
-    throw err;
+async function readDirictory() {
+  try {
+    filesList = fs.readdirSync(dataFolder);
+    console.log(`read files names`);
+  } catch (error) {
+    console.error(
+      `Got an error trying to read the files names from the dirictory: ${error.message}`
+    );
   }
-  data.forEach((file) => {
-    filesList.push(file);
-  });
-});
+}
+
+async function appendToFile(fileName, data) {
+  try {
+    fs.appendFileSync(dataFolder + fileName + ".txt", data, { flag: "w" });
+    console.log(`Appended data to ${fileName}`);
+  } catch (error) {
+    console.error(`Got an error trying to append the file: ${error.message}`);
+  }
+}
 
 app.get("/", (req, res) => {
+  readDirictory();
   res.render("index", { filesList });
 });
 
 app.get("/create", (req, res) => {
   res.render("create");
+});
+
+app.post("/create", (req, res) => {
+  appendToFile(req.body["file-name"], req.body["file-content"]);
+  readDirictory();
+  res.render("index", { filesList });
 });
 
 app.get("/files/:filename", (req, res) => {
