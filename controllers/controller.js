a5b05787823e8a0f379e92ea16d9e1f26330bac4
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-10-26 20:01:39 +0300

    file name validation

--- controllers/controller.js
+++ controllers/controller.js
@@ -60,49 +60,6 @@ function removeExtension(filename) {
   return filename.substring(0, filename.lastIndexOf(".")) || filename;
 }
 
-function showForm(filename, content) {
-  /// move to controller
-
-  document.getElementsByClassName("content")[0].innerHTML = `
-      <form action="/edit/${filename}" method="post">
-          <div>
-            <p>file Name:</p>
-            <input type="text" name="new-file-name" value = "${filename}" required />
-          </div>
-          <div>
-            <p><label for="file-content">Content:</label></p>
-            <textarea
-              id="file-content"
-              name="file-content"
-              rows="10"
-              cols="50"
-            >${content}</textarea>
-          </div>
-          <input type="submit" value="Submit" />
-        </form>
-        <form action = '/delete/${filename}' method="post">
-         <button type="submit">
-            Delete File
-          </button>
-          </form>
-        `;
-}
-
-function validateFilename(filesList1) {
-  //check for common valiadation for file name.
-  const filesList = JSON.parse(filesList1);
-  let filename = document.getElementById("file-name-id").value;
-  let filenameErr = document.getElementById("file-name-error");
-
-  if (filesList.includes(filename + ".txt")) {
-    filenameErr.innerHTML = "File already found. change the name please";
-    return false;
-  } else {
-    filenameErr.innerHTML = "";
-    return true;
-  }
-}
-
 /**Controllers */
 
 const get_home = (req, res) => {
@@ -122,9 +79,10 @@ const post_create = (req, res) => {
 };
 
 const get_file_details = (req, res) => {
+  readDirictory();
   const filename = removeExtension(req.params.filename);
   const fileContent = readFile(req.params.filename);
-  res.render("details", { filename, fileContent });
+  res.render("details", { filename, fileContent, filesList });
 };
 
 const edit_file = (req, res) => {
--- public/scripts.js
+++ public/scripts.js
@@ -1,42 +1,37 @@
-function showForm(filename, content) {
-  /// move to controller
-
-  document.getElementsByClassName("content")[0].innerHTML = `
-    <form action="/edit/${filename}" method="post">
-        <div>
-          <p>file Name:</p>
-          <input type="text" name="new-file-name" value = "${filename}" required />
-        </div>
-        <div>
-          <p><label for="file-content">Content:</label></p>
-          <textarea
-            id="file-content"
-            name="file-content"
-            rows="10"
-            cols="50"
-          >${content}</textarea>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-      <form action = '/delete/${filename}' method="post">
-       <button type="submit">
-          Delete File
-        </button>
-        </form>
-      `;
+function isValidWindowsFilename(filename) {
+  const rg1 = /^[^\\/:\*\?"<>\|]+$/; // forbidden characters \ / : * ? " < > |
+  const rg2 = /^\./; // cannot start with dot (.)
+  const rg3 = /^(nul|prn|con|lpt[0-9]|com[0-9])(\.|$)/i; // forbidden file names
+  return rg1.test(filename) && !rg2.test(filename) && !rg3.test(filename);
 }
 
 function validateFilename(filesList1) {
-  //check for common valiadation for file name.
   const filesList = JSON.parse(filesList1);
-  let filename = document.getElementById("file-name-id").value;
+  let filename = document.getElementById("file-name-id").value.trim();
   let filenameErr = document.getElementById("file-name-error");
-
+  if (filename === "") {
+    filenameErr.innerHTML = "File Name can't be empty. change the name please";
+    return false;
+  }
+  if (!isValidWindowsFilename(filename)) {
+    filenameErr.innerHTML =
+      "File Name is using forbidding characters. change the name please";
+    return false;
+  }
   if (filesList.includes(filename + ".txt")) {
     filenameErr.innerHTML = "File already found. change the name please";
     return false;
   } else {
     filenameErr.innerHTML = "";
     return true;
+  }
+}
+
+function showForm() {
+  const x = document.getElementsByClassName("content-form")[0];
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
